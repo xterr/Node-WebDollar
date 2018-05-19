8d1b3180b62af0378f422ef850cbec6e6b3dc5e1
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-05-20 02:36:49 +0300

    testnet fallbacks

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -13,26 +13,26 @@ export default {
         //{"addr": ["https://amsterdam.wdpool.io:443"]},
         //{"addr": ["https://strasbourg.wdpool.io:443"]},
         //{"addr": ["https://paris.wdpool.io:443"]},
-        {"addr": ["https://webdollar.csland.ro:443"]},
-        {"addr": ["https://nodecstl.ddns.net:80"]},
-        {"addr": ["https://webd.5q.ro:3333"]},
-        {"addr": ["https://shpool.ml:443"]}, // Thanks to @Amahte
-        {"addr": ["https://webdollar.network:5000"]}, // Thanks to @ader1990
-
-        {"addr": ["https://nodecstl.ddns.net:81/"]}, // Thanks to @taralungaCostel
-        {"addr": ["https://robitza.ddns.net:80"]}, // Thanks to @robertclaudiu
-        {"addr": ["https://robitza.ddns.net:443"]}, // Thanks to @robertclaudiu
-        {"addr": ["https://robitza.ddns.net:8080"]}, // Thanks to @robertclaudiu
-        {"addr": ["https://robitza.ddns.net:8081"]}, // Thanks to @robertclaudiu
-        {"addr": ["https://robitza.ddns.net:8082"]}, // Thanks to @robertclaudiu
-        {"addr": ["https://wd1.hoste.ro:51261"]}, // Thanks to @morion4000
-        {"addr": ["https://wd1.hoste.ro:53642"]}, // Thanks to @morion4000
-        {"addr": ["https://wd1.hoste.ro:60260"]}, // Thanks to @morion4000
-        {"addr": ["https://wd1.hoste.ro:61099"]}, // Thanks to @morion4000
-        {"addr": ["https://wd2.hoste.ro:55700"]}, // Thanks to @morion4000
-        {"addr": ["https://wd2.hoste.ro:55974"]}, // Thanks to @morion4000
-        {"addr": ["https://wd2.hoste.ro:63980"]}, // Thanks to @morion4000
-        {"addr": ["https://wd2.hoste.ro:65279"]}, // Thanks to @morion4000
+        // {"addr": ["https://webdollar.csland.ro:443"]},
+        // {"addr": ["https://nodecstl.ddns.net:80"]},
+        // {"addr": ["https://webd.5q.ro:3333"]},
+        // {"addr": ["https://shpool.ml:443"]}, // Thanks to @Amahte
+        // {"addr": ["https://webdollar.network:5000"]}, // Thanks to @ader1990
+        //
+        // {"addr": ["https://nodecstl.ddns.net:81/"]}, // Thanks to @taralungaCostel
+        // {"addr": ["https://robitza.ddns.net:80"]}, // Thanks to @robertclaudiu
+        // {"addr": ["https://robitza.ddns.net:443"]}, // Thanks to @robertclaudiu
+        // {"addr": ["https://robitza.ddns.net:8080"]}, // Thanks to @robertclaudiu
+        // {"addr": ["https://robitza.ddns.net:8081"]}, // Thanks to @robertclaudiu
+        // {"addr": ["https://robitza.ddns.net:8082"]}, // Thanks to @robertclaudiu
+        // {"addr": ["https://wd1.hoste.ro:51261"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd1.hoste.ro:53642"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd1.hoste.ro:60260"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd1.hoste.ro:61099"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd2.hoste.ro:55700"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd2.hoste.ro:55974"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd2.hoste.ro:63980"]}, // Thanks to @morion4000
+        // {"addr": ["https://wd2.hoste.ro:65279"]}, // Thanks to @morion4000
 
         //---------------------------------------------------------
         //--------------WebDollar FallBack Nodes-------------------
@@ -42,17 +42,17 @@ export default {
         {"addr": ["https://webdollar.ddns.net:8080"]},
         {"addr": ["https://webdollar.ddns.net:8081"]},
 
-        {"addr": ["https://skyhub.me:80"]},
-        {"addr": ["https://presa7.ro:80"]},
-
-        {"addr": ["https://webdollar-vps1.ddns.net:80"]},
-        {"addr": ["https://webdollar-vps2.ddns.net:80"]},
-        {"addr": ["https://webdollar-vps3.ddns.net:80"]},
-
-        {"addr": ["https://hcern.ddns.net:80"]},
-        {"addr": ["https://hcern.ddns.net:443"]},
-        {"addr": ["https://hcern.ddns.net:8080"]},
-        {"addr": ["https://hcern.ddns.net:8081"]},
+        // {"addr": ["https://skyhub.me:80"]},
+        // {"addr": ["https://presa7.ro:80"]},
+        //
+        // {"addr": ["https://webdollar-vps1.ddns.net:80"]},
+        // {"addr": ["https://webdollar-vps2.ddns.net:80"]},
+        // {"addr": ["https://webdollar-vps3.ddns.net:80"]},
+        //
+        // {"addr": ["https://hcern.ddns.net:80"]},
+        // {"addr": ["https://hcern.ddns.net:443"]},
+        // {"addr": ["https://hcern.ddns.net:8080"]},
+        // {"addr": ["https://hcern.ddns.net:8081"]},
 
     ]
 }
